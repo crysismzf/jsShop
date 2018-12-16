1443312e50f5331ddd8fbb4d8e862dc8a4216c1b
petrushenko <stas-petrushenko@mail.ru>
2018-12-16 13:48:12 +0300

    Доделана корзину и сделаны кнопки закрытия

--- js/eshop.js
+++ js/eshop.js
@@ -5,39 +5,48 @@ var CategoriaName;
 // Массив с товарами в корзине
 var basket = [];
 
+
+
 function eventSortByCostUp (e) {
 	$(".catalog__item").remove();
+	$("#ResultBasket").remove();
 	Newgoods = sort(Newgoods, sortCostUp);
 	printCatalog (Newgoods);
 };
 function eventSortByCostDown (e) {
 	$(".catalog__item").remove();
+	$("#ResultBasket").remove();
 	Newgoods = sort(Newgoods, sortCostDown);
 	printCatalog (Newgoods);
 };
 function eventSortByWeightUp(e) {
 	$(".catalog__item").remove();
+	$("#ResultBasket").remove();
 	Newgoods = sort(Newgoods, sortWeightUp);
 	printCatalog (Newgoods);
 };
 function eventSortByWeightDown(e) {
 	$(".catalog__item").remove();
+	$("#ResultBasket").remove();
 	Newgoods = sort(Newgoods, sortWeightDown);
 	printCatalog (Newgoods);
 };
 function eventSortByVogueUp(e) {
-	$(".catalog__item").remove();;
+	$(".catalog__item").remove();
+	$("#ResultBasket").remove();
 	Newgoods = sort(Newgoods, sortVogueUp);
 	printCatalog (goods);
 };
 function eventSortByVogueDown(e) {
 	$(".catalog__item").remove();
+	$("#ResultBasket").remove();
 	Newgoods = sort(Newgoods, sortVogueDown);
 	printCatalog (Newgoods);
 };
 
 function eventfilterBefore10000(e) {
 	$(".catalog__item").remove();
+	$("#ResultBasket").remove();
 	Newgoods = filt(Newgoods, filterBefore10000);
 	if (Newgoods.length == 0) {
 		Newgoods = filt(goods, filterBefore10000)
@@ -47,6 +56,7 @@ function eventfilterBefore10000(e) {
 
 function eventfilterMore10000(e) {
 	$(".catalog__item").remove();
+	$("#ResultBasket").remove();
 	Newgoods = filt(Newgoods, filterMore10000);
 	if (Newgoods.length == 0) {
 		Newgoods = filt(goods, filterMore10000)
@@ -57,6 +67,7 @@ function eventfilterMore10000(e) {
 function eventfilterCategoria(e) {
 	CategoriaName = $(this).text();
 	$(".catalog__item").remove();
+	$("#ResultBasket").remove();
 	Newgoods = filt(Newgoods, filterCategories);
 	if (Newgoods.length == 0) {
 		Newgoods = filt(goods,  filterCategories)
@@ -80,23 +91,32 @@ function eventShowDiscription(e) {
 	 collectionInfo(goods);
 	 $(".catalog").prepend("<div class =\"catalog__item\"> <span class =\"catalog__item-name\">"+NameOfProduct+
 	 "</span><img class =\"catalog__item-preview\" src = \"images/im1.svg\"> <span class =\"catalog__item-cost\">"+costOfProduct+
-	 "</span>"+discriptionOfProduct+"</div>");	
+	 "</span> Описание товара:</br>"+discriptionOfProduct+"<a href=\"#\" id = \"closeDiscription\"> Закрыть </a></div>");	
 	
 };
 // Отрисовка корзины
 function eventShowBasket (e) {
 	
 	$(".catalog__item").remove();
+	var totalWeight =0;
+	var totalPrice = 0;
+			for (var j=0; j<basket.length; j++) {
+				totalPrice += basket[j].cost;
+				totalWeight += basket[j].weight;
+			};
 	$(".catalog").prepend("<table id=\"ResultBasket\"><tr><td>Наименование</td><td>Категория</td><td>Вес</td><td>Цена</td><td>Удалить</td></tr></table>");
 	for (var i=0; i<basket.length; i++) {
 		$("tbody").append("<tr><td>"+basket[i].name+"</td><td>"+basket[i].category+"</td><td>"+basket[i].weight+
-		"</td><td>"+basket[i].cost+"</td><td><a href=\"#\" id=\"DeleteFromBacket\">-</a></td></tr>");	 
-	}
+		"</td><td>"+basket[i].cost+"</td><td><a href=\"#\" id=\"DeleteFromBacket\">Х</a></td></tr>");	 
+	};
+	$("tbody").append("<tr><td> Общий вес товаров </td> <td>"+totalWeight+"</td> <td> На сумму </td><td>"+totalPrice+
+	"</td></tr>"); 
+	$("tbody").append("</br> <button> Купить </button> <a href=\"#\" id=\"closeBasket\"> Закрыть корзину </a>")
 	
-}
+};
 
 
-
+// удаление из корзины
 function eventDeleteFromBacket (e) {
 	NameOfProduct = $(this).parent().siblings().eq(0).text();
 	for (var i=0; i<basket.length; i++) {
@@ -104,12 +124,24 @@ function eventDeleteFromBacket (e) {
 		if (NameOfProduct == basket[i].name) {
 			basket.splice(i,1);
 			break;
-		}
-		
+		};
+	
 	};
+	var totalPrice = 0;
+			for (var j=0; j<basket.length; j++) {
+				totalPrice += basket[j].cost;
+			};	
+	$("#backet").text("У вас в корзине "+basket.length+" товаров на общую сумму "+totalPrice);
 	$("#ResultBasket").remove();
 	$("#backet").trigger("click");
 };
+
+function eventCloseBacket(e) {
+	$("#ResultBasket").remove();
+	printCatalog (Newgoods);
+	
+};
+
 
 
 $(function(e) {
@@ -185,9 +217,20 @@ $(function(e) {
 	 
 	// Отрисовка корзины
 		$("#backet").click(eventShowBasket); 
-		
+	// Удаление из корзины 	
 	$(document).on('click','#DeleteFromBacket', eventDeleteFromBacket);	
-		
+	
+	// Закрытие корзины 	
+	$(document).on('click','#closeBasket', eventCloseBacket);	
+	// Закрытие описания товара
+	$(document).on('click','#closeDiscription',function(event){
+			event.stopPropagation();
+			$(".catalog__item").remove();
+			printCatalog (Newgoods);
+			
+					
+	});
+	
 	});
 
 });	
@@ -256,6 +299,7 @@ $(function(e) {
 	
 	// Функция сброса фильторов и сортировки
 	function resetFilter() {
+		$("#ResultBasket").remove();
 		$(".catalog__item").remove();
 		printCatalog (goods);
 		
